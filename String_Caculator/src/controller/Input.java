d88a24e7b4c8fd3d04a48fb5b11c7409925dba38
Uangi <jyp423@naver.com>
2023-12-03 14:13:52 +0900

    git test

---
+++ String_Caculator/src/controller/Input.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class Input {
+	
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
---
+++ String_Caculator/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jyp42
+ *
+ */
+module String_Caculator {
+}
